20d767bcee25a8cfe0d2918cec64bee7db99d900
giuseppemirizzi <giuseppe.m1995@gmail.com>
2022-03-14 13:43:35 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+const todos = [
+    {
+        todo:'Lavarsi la faccia',
+        done:false
+    },
+    {
+        todo:'Fare colazione',
+        done:false
+    },
+    {
+        todo:'Lavarsi i denti',
+        done:false
+    }
+]
