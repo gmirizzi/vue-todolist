532c1adf1a8e22342a2a9dfc821e8996164bc49a
giuseppemirizzi <giuseppe.m1995@gmail.com>
2022-03-14 15:13:05 +0100

    bonus

--- script.js
+++ script.js
@@ -1,25 +1,36 @@
-const app = new Vue (
+const app = new Vue(
     {
-        el:'#root',
+        el: '#root',
         data: {
+            newToDo: "",
             todos: [
                 {
-                    text:'Lavarsi la faccia',
-                    done:false
+                    text: 'Lavarsi la faccia',
+                    done: false
                 },
                 {
-                    text:'Fare colazione',
-                    done:false
+                    text: 'Fare colazione',
+                    done: false
                 },
                 {
-                    text:'Lavarsi i denti',
-                    done:false
+                    text: 'Fare la cacca',
+                    done: false
                 }
             ]
         },
         methods: {
-            deleteToDo(index){
-                this.todos.splice(index,1)
+            deleteToDo(index) {
+                this.todos.splice(index, 1)
+            },
+            addToDo() {
+                if (this.newToDo.trim() != "") {
+                    const newElement={
+                        text: this.newToDo.trim(),
+                        done:false
+                    }
+                    this.todos.push(newElement)
+                }
+                this.newToDo=""
             }
         }
     }
